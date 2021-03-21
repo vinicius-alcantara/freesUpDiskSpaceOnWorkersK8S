083af9bd4403a166d5322f90f4e1f6f2d719b6f1
vinicius-alcantara <vinicius.redes2011@gmail.com>
2021-03-21 19:22:48 -0300

    update

--- script.sh
+++ script.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 ############### VARS ################
 USER="vinicius";
-WORKDIR="$(cd ~ && pwd)";
+CURRENT_LOCAL_USER="$(cd ~ && pwd)";
+WORKDIR_NAME="freesUpDiskSpaceOnWorkersK8S";
+WORKDIR_FULL_PATH="$(echo "$CURRENT_LOCAL_USER"/"$WORKDIR_NAME")";
+SEND_EMAIL_SCRIPT_NAME="sendMailCurlFunction.sh";
+SCRIPT_MAIN_NAME="script.sh";
+SEND_EMAIL_SCRIPT_FULL_PATH="$(echo "$WORKDIR_FULL_PATH"/"$SEND_EMAIL_SCRIPT_NAME")";
+SCRIPT_MAIN_NAME_FULL_PATH="$(echo "$WORKDIR_FULL_PATH"/"$SCRIPT_MAIN_NAME")";
 CURRENT_TIME=$(date +%H);
-SENDMAIL_FUNCTION_INCLUDE="$(echo "$WORKDIR"/sendMailCurlFunction.sh)";
 #####################################
 ######## INCLUDES | IMPORTS  ########
-source "$SENDMAIL_FUNCTION_INCLUDE";
+source "$SEND_EMAIL_SCRIPT_FULL_PATH";
 ##################################### 
 
 function drainWorkersK8S(){
@@ -17,71 +22,97 @@ do
 	WORKER_NAME=$(echo "$WORKER_NAME_AND_IP_CREATE_VARS" | tr "_" "-");
 	WORKER_IP=$(kubectl get node "$WORKER_NAME" -o wide | egrep -v master | cut -c43-56 | sed 1d | sed 's/ //g');	
 	ENVIRONMENT=$(kubectl get nodes "$WORKER_NAME" -L environment | sed 1d | sed 's/ //g' | cut -d "-" -f2);
-	DISK_ESPACE_IN_USE_PERCENT=$(ssh "$USER"@"$WORKER_IP" df -h / --output=pcent | tail -n1 | sed 's/ //g' | sed 's/%//');
+	DISK_SPACE_IN_USE_PERCENT=$(ssh "$USER"@"$WORKER_IP" df -h / --output=pcent | tail -n1 | sed 's/ //g' | sed 's/%//');
+	DISK_SIZE_OLD=$(ssh "$USER"@"$WORKER_IP" df -h / --output=size | tail -n1);
+	DISK_USED_OLD=$(ssh "$USER"@"$WORKER_IP" df -h / --output=used | tail -n1);
+	DISK_AVAIL_OLD=$(ssh "$USER"@"$WORKER_IP" df -h / --output=avail | tail -n1);
+	DISK_PCENT_OLD=$(ssh "$USER"@"$WORKER_IP" df -h / --output=pcent | tail -n1);
 
-	if [ "$DISK_ESPACE_IN_USE_PERCENT" -ge 30 ] && [ "$ENVIRONMENT" == "PRODUCAO" ] && [ "$CURRENT_TIME" -ge 20 ]; 
+	if [ "$DISK_SPACE_IN_USE_PERCENT" -ge 30 ] && [ "$ENVIRONMENT" == "PRODUCAO" ]; #&& [ "$CURRENT_TIME" -ge 20 ]; 
 	then
-		#echo "O $WORKER_NAME é tagueado para $ENVIRONMENT";
-	   	kubectl drain $WORKER_NAME --force --timeout=500s --delete-local-data --ignore-daemonsets;
+	   	kubectl drain $WORKER_NAME --force --timeout=500s --delete-emptydir-data --ignore-daemonsets;
 		if [ $? == 0 ];
 		then
 		     UNSCHEDULABLE=$(kubectl describe no $WORKER_NAME | egrep "Unschedulable" | sed 's/ //g' | cut -d ":" -f2);
-		     if [ $UNSCHEDULABLE == "true"];
+		     if [ $UNSCHEDULABLE == "true" ];
 		     then
-		     	kubectl uncordon $WORKER_NAME;
+		     	 kubectl uncordon $WORKER_NAME;
+			 if [ $? != 0 ];
+			 then
+			     UNSCHEDULABLE=$(kubectl describe no $WORKER_NAME | egrep "Unschedulable" | sed 's/ //g' | cut -d ":" -f2);
+			     if [ $UNSCHEDULABLE != "true" ];
+			     then
+			         function sendEmailNotificationFailedCode2(){
+                                     sendEmailNotificationFailedUncordon;
+                                 }
+                                  sendEmailNotificationFailedCode2;
+                                  exit 0;
+			     fi
+			 fi
+		     else
+			 # Drain Unchedulable
+			 function sendEmailNotificationFailedCode4(){
+                             sendEmailNotificationFailedDrainUnschedulableFalse;
+                         }
+                          sendEmailNotificationFailedCode4;
+                          exit 0;
 		     fi
-			
+		    
 			UNSCHEDULABLE=$(kubectl describe no $WORKER_NAME | egrep "Unschedulable" | sed 's/ //g' | cut -d ":" -f2);
 			if [ $UNSCHEDULABLE == "false" ];
 			then
 				
-			    ssh "$USER"@"$WORKER_IP" systemctl restart docker && DOCKER_DAEMON_STATUS=$(ssh "$USER"@"$WORKER_IP" systemctl status docker | egrep -i "active" | cut -d ")" -f1 | cut -d "(" -f1 | cut -d ":" -f2 | sed 's/ //g');
+			    ssh -t "$USER"@"$WORKER_IP" sudo systemctl restart docker && DOCKER_DAEMON_STATUS=$(ssh "$USER"@"$WORKER_IP" sudo systemctl status docker | egrep -i "active" | cut -d ")" -f1 | cut -d "(" -f1 | cut -d ":" -f2 | sed 's/ //g');
 			  
 			    if [ $? == 0 ] && [ $DOCKER_DAEMON_STATUS == "active" ];
 			    then
+				ssh -t "$USER"@"$WORKER_IP" sudo docker image prune -af;
+				if [ $? == 0 ];
+				then
+				    DISK_SIZE_NEW=$(ssh "$USER"@"$WORKER_IP" df -h / --output=size | tail -n1);
+			            DISK_USED_NEW=$(ssh "$USER"@"$WORKER_IP" df -h / --output=used | tail -n1);
+        			    DISK_AVAIL_NEW=$(ssh "$USER"@"$WORKER_IP" df -h / --output=avail | tail -n1);
+        			    DISK_PCENT_NEW=$(ssh "$USER"@"$WORKER_IP" df -h / --output=pcent | tail -n1);
 
-				docker image prune -af;
-				function sendEmailNotificationSuccess(){
-				    sendEmailNotificationSuccess;
-	
-                                }
+				    function sendEmailNotificationSuccess(){
+                                    sendEmailNotificationCompleteSuccess;
+                                    }
+                                     sendEmailNotificationSuccess;
+                                     exit 0;
 
-				  sendEmailNotificationSuccess;
-
+			        else
+				    echo "Falha ao realizar prune image";
+				    exit 0;
+				fi
+				
 			    else
-
 				function sendEmailNotificationFailedCode1(){
 				    sendEmailNotificationFailedStatusDocker;					
                                 }   
-				  sendEmailNotificationFailedCode1;
-			    fi			    
-			    
-			else
-
+				 sendEmailNotificationFailedCode1;
+				 exit 0;
+			    fi
+		        else
 			    function sendEmailNotificationFailedCode2(){
-				sendEmailNotificationFailedUncordon;
-			    } 				
-			      sendEmailNotificationFailedCode2;
-			fi
-	                 
-		else
+                                sendEmailNotificationFailedUncordon;
+                            }
+                             sendEmailNotificationFailedCode2;
+			     exit 0;
+   		        fi			       
+	        else
 		    function sendEmailNotificationFailedCode3(){
-                         sendEmailNotificationFailedDrain;
+                        sendEmailNotificationFailedDrain;
                     }
-		      sendEmailNotificationFailedCode3;
-		fi		
-
-
-	elif [ "$DISK_ESPACE_IN_USE_PERCENT" -ge 5 ] && [ "$ENVIRONMENT" == "DESENVOLVIMENTO" ];
-	then	
-		#echo "O $WORKER_NAME é tagueado para $ENVIRONMENT";
-		
-	fi
-
+		     sendEmailNotificationFailedCode3;
+		     exit 0;
+		fi
+	fi	     
 	
 done
 
 }
 
 drainWorkersK8S;
+
+
 
--- sendMailCurlFunction.sh
+++ sendMailCurlFunction.sh
@@ -2,17 +2,32 @@
 ###### VARs - CONFIG AND ACESS's ######
 SMTP_SRV="smtp.office365.com";
 SMTP_PORT="587";
-SMTP_USR="$(echo -ne "yyyyyyyyyy" | base64 -d)";
-SMTP_PASS="$(echo -ne "xxxxxxxxx" | base64 -d)"
+SMTP_USR="$(echo -ne "dmluaWNpdXMuYWxjYW50YXJhQHNvbW9zYWdpbGl0eS5jb20uYnIK" | base64 -d)";
+SMTP_PASS="$(echo -ne "TXVkYXJAMTIzCg==" | base64 -d)"
 MAIL_FROM="$(echo -ne "dmluaWNpdXMuYWxjYW50YXJhQHNvbW9zYWdpbGl0eS5jb20uYnI=" | base64 -d)";
 MAIL_TO_1="vinicius.redes2011@gmail.com";
 MAIL_TO_2="vinicius.redes2020@gmail.com";
-SUBJECT_SUCCESS="SUCCESS: SRM-SRV-";
-SUBJECT_FAILED="FAILED: SRM-SRV-";
-SUCCESS_NAME_FILE_EMAIL="body_mail_success.txt";
+SUBJECT_SUCCESS="SUCCESS: SRM-SRV";
+SUBJECT_FAILED="FAILED: SRM-SRV";
+SUCCESS_NAME_FILE_EMAIL="body_mail_success.html";
+SUCCESS_NAME_FILE_EMAIL_MODIFY="body_mail_success_modify.txt";
+DIR_TEMPLATE_BODY_EMAILS="templates_emails";
+#TR_COMMAND="$(which tr)";
+cd $WORKDIR_FULL_PATH;
+if [ $? == 0 ];
+then
+    if [ ! -e $DIR_TEMPLATE_BODY_EMAILS ];
+    then
+	mkdir $DIR_TEMPLATE_BODY_EMAILS;
+    fi	    
+else
+    echo "Falha ao acessar o diretório raiz do projeto - WORKDIR.";
+    exit 0;
+fi
 FAILED_NAME_FILE_EMAIL_CODE_1="body_mail_failed1.txt";
 FAILED_NAME_FILE_EMAIL_CODE_2="body_mail_failed2.txt";
 FAILED_NAME_FILE_EMAIL_CODE_3="body_mail_failed3.txt";
+FAILED_NAME_FILE_EMAIL_CODE_4="body_mail_failed4.txt";
 CURRENT_TIME_LOCAL="$(date +%H)";
 
 if [ "$CURRENT_TIME_LOCAL" -ge 0 ] && [ "$CURRENT_TIME_LOCAL" -lt 12 ];
@@ -31,22 +46,34 @@ then
     INITIAL_MESSAGE_BODY_MAIL="boa noite";
 
 fi
+
 #######################################
 #######################################
 ########### VARs - MESSAGES ###########
 BODY_MAIL_SUCCESS="
 From: "$MAIL_FROM"
-To: "$MAIL_TO_1" "$MAIL_TO_2"
-Subject: "$SUBJECT_SUCCESS"
+To: "$MAIL_TO_1" 
+Cc: "$MAIL_TO_2"
+Subject: "$SUBJECT_SUCCESS"-<WORKER_NAME>
 
 Olá, "$INITIAL_MESSAGE_BODY_MAIL",
-Espaço em disco liberado com sucesso no servidor/worker mencionado no assunto deste e-mail.
+Espaço em disco liberado com sucesso no servidor/worker mencionado no assunto deste e-mail. Segue Evidências.
+
+Antes:
+
+Filesystem                       			Size Used Avail Use% Mounted
+/dev/mapper/ubuntu--vg-ubuntu--lv      <DISK_SIZE_OLD><DISK_USED_OLD><DISK_AVAIL_OLD> <DISK_PCENT_OLD>  	/
+
+Depois:
+
+Filesystem                                              Size Used Avail Use% Mounted
+/dev/mapper/ubuntu--vg-ubuntu--lv      <DISK_SIZE_NEW><DISK_USED_NEW><DISK_AVAIL_NEW> <DISK_PCENT_NEW>     /
 ";
 #######################################
 BODY_MAIL_FAILED_CODE_1="
 From: "$MAIL_FROM"
 To: "$MAIL_TO_1" "$MAIL_TO_2"
-Subject: "$SUBJECT_FAILED"
+Subject: "$SUBJECT_FAILED"-<WORKER_NAME>
 
 Olá, "$INITIAL_MESSAGE_BODY_MAIL",
 Falha ao tentar liberar espaço em disco no servidor/worker mencionado no assunto deste e-mail.
@@ -57,7 +84,7 @@ Por favor, verificar.
 BODY_MAIL_FAILED_CODE_2="
 From: "$MAIL_FROM"
 To: "$MAIL_TO_1" "$MAIL_TO_2"
-Subject: "$SUBJECT_FAILED"
+Subject: "$SUBJECT_FAILED"-<WORKER_NAME>
 
 Olá, "$INITIAL_MESSAGE_BODY_MAIL",
 Falha ao tentar liberar espaço em disco no servidor/worker mencionado no assunto deste e-mail. 
@@ -68,7 +95,7 @@ Por favor, verificar.
 BODY_MAIL_FAILED_CODE_3="
 From: "$MAIL_FROM"
 To: "$MAIL_TO_1" "$MAIL_TO_2"
-Subject: "$SUBJECT_FAILED"
+Subject: "$SUBJECT_FAILED"-<WORKER_NAME>
 
 Olá, "$INITIAL_MESSAGE_BODY_MAIL",
 Falha ao tentar liberar espaço em disco no servidor/worker mencionado no assunto deste e-mail. 
@@ -76,44 +103,64 @@ Error: Falha ao realizar drain no worker. Provável ocorrência de timeout ao re
 Por favor, verificar
 ";
 #######################################
+BODY_MAIL_FAILED_CODE_4="
+From: "$MAIL_FROM"
+To: "$MAIL_TO_1" "$MAIL_TO_2"
+Subject: "$SUBJECT_FAILED"-<WORKER_NAME>
+
+Olá, "$INITIAL_MESSAGE_BODY_MAIL",
+Falha ao tentar liberar espaço em disco no servidor/worker mencionado no assunto deste e-mail. 
+Error: Falha ao realizar drain no worker. O worker permanece schedulando novos pods (Unschedulable = false).
+Por favor, verificar
+";
+#######################################
 
 function create_body_mail_success() {
-	cd ~;
-	CURRENT_LOCAL=$(pwd);
-	echo "$BODY_MAIL_SUCCESS" | sed 1d > "$CURRENT_LOCAL"/"$SUCCESS_NAME_FILE_EMAIL";  
-
+	cd "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS";
+	echo "$BODY_MAIL_SUCCESS" | sed 1d > "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";  
 }
 
 create_body_mail_success;
 
 function create_body_mail_failed_code_1() {
-        cd ~;
-        CURRENT_LOCAL=$(pwd);
-        echo "$BODY_MAIL_FAILED_CODE_1" | sed 1d > "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_1";
-
+        cd "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS";
+        echo "$BODY_MAIL_FAILED_CODE_1" | sed 1d > "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_1";
 }
 
 create_body_mail_failed_code_1;
 
 function create_body_mail_failed_code_2() {
-        cd ~;
-        CURRENT_LOCAL=$(pwd);
-        echo "$BODY_MAIL_FAILED_CODE_2" | sed 1d > "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_2";
-
+        cd "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS";
+        echo "$BODY_MAIL_FAILED_CODE_2" | sed 1d > "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_2";
 }
 
 create_body_mail_failed_code_2;
 
 function create_body_mail_failed_code_3() {
-        cd ~;
-        CURRENT_LOCAL=$(pwd);
-        echo "$BODY_MAIL_FAILED_CODE_3" | sed 1d > "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_3";
-
+        cd "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS";
+        echo "$BODY_MAIL_FAILED_CODE_3" | sed 1d > "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_3";
 }
 
 create_body_mail_failed_code_3;
 
-function sendEmailNotificationSuccess() {
+function create_body_mail_failed_code_4() {
+        cd "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS";
+        echo "$BODY_MAIL_FAILED_CODE_4" | sed 1d > "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_4";
+}
+
+create_body_mail_failed_code_4;
+
+function sendEmailNotificationCompleteSuccess() {
+
+  sed -i s/"<WORKER_NAME>"/"$WORKER_NAME"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_SIZE_OLD>"/"$DISK_SIZE_OLD"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_USED_OLD>"/"$DISK_USED_OLD"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_AVAIL_OLD>"/"$DISK_AVAIL_OLD"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_PCENT_OLD>"/"$DISK_PCENT_OLD"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_SIZE_NEW>"/"$DISK_SIZE_NEW"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_USED_NEW>"/"$DISK_USED_NEW"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_AVAIL_NEW>"/"$DISK_AVAIL_NEW"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+  sed -i s/"<DISK_PCENT_NEW>"/"$DISK_PCENT_NEW"/g "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
 
   curl --ssl-reqd \
     --url "$SMTP_SRV":"$SMTP_PORT" \
@@ -121,16 +168,16 @@ function sendEmailNotificationSuccess() {
     --mail-from "$MAIL_FROM" \
     --mail-rcpt "$MAIL_TO_1" \
     --mail-rcpt "$MAIL_TO_2" \
-    --upload-file "$CURRENT_LOCAL"/"$SUCCESS_NAME_FILE_EMAIL";
+    --upload-file "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL"
 
-  if [ -e "$CURRENT_LOCAL"/"$SUCCESS_NAME_FILE_EMAIL" ]; 
-  then
-     rm -rf "$CURRENT_LOCAL"/"$SUCCESS_NAME_FILE_EMAIL";
-  fi
+#  if [ -e "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL" ]; 
+#  then
+#     rm -rf "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$SUCCESS_NAME_FILE_EMAIL";
+#  fi
 
 }
 
-#sendEmailNotificationSuccess;
+#sendEmailNotificationCompleteSuccess;
 
 function sendEmailNotificationFailedStatusDocker() {
 
@@ -140,11 +187,11 @@ function sendEmailNotificationFailedStatusDocker() {
     --mail-from "$MAIL_FROM" \
     --mail-rcpt "$MAIL_TO_1" \
     --mail-rcpt "$MAIL_TO_2" \
-    --upload-file "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_1";
+    --upload-file "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_1";
 
-  if [ -e "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_1" ];
+  if [ -e "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_1" ];
   then
-     rm -rf "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_1";
+     rm -rf "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_1";
   fi
 
 }
@@ -159,11 +206,11 @@ function sendEmailNotificationFailedUncordon() {
     --mail-from "$MAIL_FROM" \
     --mail-rcpt "$MAIL_TO_1" \
     --mail-rcpt "$MAIL_TO_2" \
-    --upload-file "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_2";
+    --upload-file "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_2";
 
-  if [ -e "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_2" ];
+  if [ -e "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_2" ];
   then
-     rm -rf "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_2";
+     rm -rf "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_2";
   fi
 
 }
@@ -178,13 +225,33 @@ function sendEmailNotificationFailedDrain() {
     --mail-from "$MAIL_FROM" \
     --mail-rcpt "$MAIL_TO_1" \
     --mail-rcpt "$MAIL_TO_2" \
-    --upload-file "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_3";
+    --upload-file "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_3";
 
-  if [ -e "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_3" ];
+  if [ -e "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_3" ];
   then
-     rm -rf "$CURRENT_LOCAL"/"$FAILED_NAME_FILE_EMAIL_CODE_3";
+     rm -rf "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_3";
   fi
 
 }
 
 #sendEmailNotificationFailedDrain;
+
+function sendEmailNotificationFailedDrainUnschedulableFalse() {
+
+  curl --ssl-reqd \
+    --url "$SMTP_SRV":"$SMTP_PORT" \
+    --user "$SMTP_USR":"$SMTP_PASS" \
+    --mail-from "$MAIL_FROM" \
+    --mail-rcpt "$MAIL_TO_1" \
+    --mail-rcpt "$MAIL_TO_2" \
+    --upload-file "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_4";
+
+  if [ -e "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_4" ];
+  then
+     rm -rf "$WORKDIR_FULL_PATH"/"$DIR_TEMPLATE_BODY_EMAILS"/"$FAILED_NAME_FILE_EMAIL_CODE_4";
+  fi
+
+}
+
+#sendEmailNotificationFailedDrainUnschedulableFalse;
+
